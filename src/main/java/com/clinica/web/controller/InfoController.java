8a37e9cac98bdee2bd1ffe28021f4c18a20f02e6
Ilie Ioana Laura <ilie_ioana_laura@yahoo.com>
2025-11-13 16:25:44 +0200

    some styling for home and redirect to other pages for the tables

---
+++ src/main/java/com/clinica/web/controller/InfoController.java
@@ -0,0 +1,13 @@
+package com.clinica.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoController {
+
+    @GetMapping("/info")
+    public String showInfoPage() {
+        return "info"; // Se leagă de templates/info.html
+    }
+}
